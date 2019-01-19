65eb58133923d528956e51b1fdff2bc428bf1563
mohammadnawazsharief <mohammadnawazsharief@users.noreply.github.com>
2019-01-19 17:11:23 +0530

    GitHub authentication

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'http://localhost/github/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => '61c21e461f74ce03fb01',
+            'client_secret' => '699a60be8b7cfd5fdc71a1ac4712db52b56ffd0c',
+            'redirect_uri' => 'http://localhost/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
